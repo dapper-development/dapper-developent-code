e7ccf20d921459e139b8827d221f296bba8f9513
dapper-development <59849638+victor-jaimes-puente@users.noreply.github.com>
2022-05-22 04:26:21 -0500

    added db connect

--- blog/dapper-blog-next/nextjs-blog-app-with-mongodb/next.config.js
+++ blog/dapper-blog-next/nextjs-blog-app-with-mongodb/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MONGO_URI: 'mongodb://localhost:8000/blogDbOne?retryWrites=true&w=majority'
+  }
 }
 
 module.exports = nextConfig
