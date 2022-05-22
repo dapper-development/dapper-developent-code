561fa5ea96abf1c6424d9b35e6dc02972d128624
dapper-development <59849638+victor-jaimes-puente@users.noreply.github.com>
2022-05-22 03:33:19 -0500

    working on dapper blog

---
+++ blog/dapper-blog-next/nextjs-blog-app-with-mongodb/pages/about.js
@@ -0,0 +1,14 @@
+import { Paper } from '@material-ui/core'
+import React from 'react' 
+
+const about = () => {
+return (
+    <div>
+        <h2>
+            Dapper-Blog
+        </h2>
+    </div>
+)
+}
+
+export default about;
